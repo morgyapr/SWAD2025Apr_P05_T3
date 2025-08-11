b709ac11966e4182132c4662a7d65a8edab9c660
morg <morgenyap@gmail.com>
2025-08-11 18:54:01 +0800

    Create CartItem.cs

---
+++ CartItem.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderAlReady.Models
+{
+    public class CartItem
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
